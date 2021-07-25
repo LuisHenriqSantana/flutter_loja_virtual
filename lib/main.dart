1afe5b19945f33290ff12cc10573fea3aed9633e
Luis <luis.henrique8000@gmail.com>
2021-07-25 01:36:24 -0300

    add provider and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/base_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja do Luis',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container()
+      home: BaseScreen()
     );
   }
 }
---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_drawer.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen extends StatelessWidget {
+
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        children: <Widget>[
+          Scaffold(
+            drawer: const CustomDrawer(),
+            appBar: AppBar(title: const Text('Home'),),
+          ),
+          Container(color: Colors.red,),
+          Container(color: Colors.green,),
+          Container(color: Colors.yellow,),
+        ],
+      ),
+    );
+  }
+}
